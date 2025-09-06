c5fbafab7dd00ff2bc4af0c827f610dc03ccc650
lalaandco <lalaandc2025@gmail.com>
2025-09-06 19:26:50 +0800

    Delete Lalal&Co.UserSystem directory

--- Lalal&Co.UserSystem/script.js
+++
@@ -1,8 +0,0 @@
-function showForm (formId){
-    document.querySelectorAll(".form-box").forEach(form => form.classList.remove("active"));
-    document.getElementById(formId).classList.add("active");
-}
-function block_backround(login) {
-    document.querySelector('.overlay').style.display = 'block';
-}
-
